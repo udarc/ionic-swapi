00ba060431190c84550427d962f486553d308f1c
udarc <46758935+udarc@users.noreply.github.com>
2019-05-06 19:11:54 -0500

    All planets recursively

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { merge,race } from 'rxjs';
+import { merge,race, empty } from 'rxjs';
+import {expand}  from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,21 @@ export class SwapiService {
 
   constructor(private httpSvc:HttpClient) { }
   getPlanets(){
-    const p1 = this.httpSvc.get("https:/swapi.co/api/planets");
-    const p2 = this.httpSvc.get("https:/swapi.co/api/planets/?page=2");
-    const p3 = this.httpSvc.get("https:/swapi.co/api/planets/?page=3");
-    const p4 = this.httpSvc.get("https:/swapi.co/api/planets/?page=4");
-    const p5 = this.httpSvc.get("https:/swapi.co/api/planets/?page=5");
-    const p6 = this.httpSvc.get("https:/swapi.co/api/planets/?page=6");
-    const p7 = this.httpSvc.get("https:/swapi.co/api/planets/?page=7");
+    // const p1 = this.httpSvc.get("https:/swapi.co/api/planets");
+    // const p2 = this.httpSvc.get("https:/swapi.co/api/planets/?page=2");
+    // const p3 = this.httpSvc.get("https:/swapi.co/api/planets/?page=3");
+    // const p4 = this.httpSvc.get("https:/swapi.co/api/planets/?page=4");
+    // const p5 = this.httpSvc.get("https:/swapi.co/api/planets/?page=5");
+    // const p6 = this.httpSvc.get("https:/swapi.co/api/planets/?page=6");
+    // const p7 = this.httpSvc.get("https:/swapi.co/api/planets/?page=7");
     // return merge(p1,p2,p3,p4,p5,p6,p7);
-    return race(p1,p2,p3,p4,p5,p6,p7)
+    // return race(p1,p2,p3,p4,p5,p6,p7);
 
+  return this.httpSvc.get("https:/swapi.co/api/planets").pipe(
+    expand(data =>
+      data.next ? this.httpSvc.get(data.next) : empty()
+    ));
+
+    }
   }
-}
+
